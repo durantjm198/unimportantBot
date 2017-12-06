db70ba507c315276c77971a7f7e5440bc6ffcc7c
durantjm198 <durantjm198@potsdam.edu>
2017-12-05 23:08:58 -0500

    channels exist

--- UnimportantBot.py
+++ UnimportantBot.py
@@ -37,7 +37,7 @@ async def on_message(message):
   is_tweet = message.content.startswith('https://twitter.com') or \
              message.content.startswith('https://www.twitter.com')
   if is_tweet:
-    return await bot.say('That looks like a tweet!')
+    return await bot.send_message(message.channel, 'That looks like a tweet!')
 
 ################################ MISCELLANY ###################################
 
