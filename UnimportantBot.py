f7c3547ba029d2bb7301b10d28ae3b578d1f9a5d
Jarred Durant <jarred@hireware.com>
2018-01-01 10:59:31 -0500

    process_commands

--- UnimportantBot.py
+++ UnimportantBot.py
@@ -20,4 +20,18 @@ class UnimportantBot(Bot):
         traceback.print_exc()
     super().run(config.token)
 
+    async def process_commands(self, message):
+      ctx = await self.get_context(message, cls=commands.Context)
+
+      if ctx.command is None:
+        return
+
+      async with ctx.acquire():
+        await self.invoke(ctx)
+
+    async def on_message(self, message):
+      if message.author.bot:
+        return
+      await self.process_commands(message)
+
 bot = UnimportantBot()
