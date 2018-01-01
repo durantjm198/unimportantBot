6e56cd7fa084a56222c1853e4b88f7fc7fe3ff4b
Jarred Durant <jarred@hireware.com>
2018-01-01 11:58:18 -0500

    twitter first pass

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -7,12 +7,19 @@ import tweepy
 class Twitter:
   def __init__(self, bot):
     self.bot = bot
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    self.api = tweepy.API(auth)
 
   async def on_message(self, message):
     is_tweet = message.content.startswith('https://twitter.com') or \
              message.content.startswith('https://www.twitter.com')
     if is_tweet:
-      return #await self.bot.send_message(get_image_links(message))
+      return await self.bot.send_message(get_image_links(message))
+
+  def get_image_links(message):
+    return self.api.get_status(938188434195341313).text
+
 
 def setup(bot):
     bot.add_cog(Twitter(bot))
--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 import os
 
 token = os.environ['discord_api_key']
+tw_consumer_key = os.environ['twitter_consumer_key']
+tw_consumer_secret = os.environ['twitter_consumer_secret']
+tw_access_token = os.environ['twitter_access_token']
+tw_access_token_secret = os.environ['twitter_access_token_secret']
