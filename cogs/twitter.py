98fceed33dec42cc71fba37592b878379a4a0874
durantjm198 <durantjm198@potsdam.edu>
2018-01-01 19:22:24 -0500

    Another silly colon

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -20,7 +20,7 @@ class Twitter:
     tweet = self.get_tweet(message)
     quote = self.get_quote(tweet)
     links = self.get_media_links(tweet)
-    if quote || links
+    if quote or links:
       return quote + '\n' + links
      
   def get_tweet(self, message):
