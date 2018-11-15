be7f8d2df5c34f3f790535ce81ce0bb1165373c1
jweisberg <jacobgweisberg@gmail.com>
2018-11-15 17:41:35 -0500

    Add files via upload

--- UnimportantBot.py
+++ UnimportantBot.py
@@ -31,6 +31,9 @@ class UnimportantBot(commands.Bot):
       server = message.server
       if message.author.bot or '!nobot' in message.content:
         return
+      if '69' in message.content:
+          nice = next((e for emojis in server.emojis if e.name == 'nice'), None)
+          await self.add_reaction(message, nice)
       await self.process_commands(message)
 
 bot = UnimportantBot()
