05ba1bf3df0d523590ab21add9e502e8a0050962
Jarred Durant <jarreddurant14@gmail.com>
2018-11-15 20:27:11 -0500

    Update UnimportantBot.py

--- UnimportantBot.py
+++ UnimportantBot.py
@@ -32,7 +32,7 @@ class UnimportantBot(commands.Bot):
     if message.author.bot or '!nobot' in message.content:
       return
     if '69' in message.content:
-      nice = next(e for e in server.emojis if e.name == 'nice', None)
+      nice = next((e for e in server.emojis if e.name == 'nice'), None)
       await self.add_reaction(message, nice)
     await self.process_commands(message)
 
